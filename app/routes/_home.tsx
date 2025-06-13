e5d090af2fd2ff627021ad7f4034ff0ad61afdac
zzz-ubuntu <imbxyz1@gmail.com>
2025-06-12 20:38:08 -0700

    fresh air

--- app/routes/_home.tsx
+++ app/routes/_home.tsx
@@ -1,12 +1,37 @@
 import type { Route } from "./+types/_home";
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 import {
   SidebarProvider,
   SidebarInset,
   SidebarTrigger,
 } from "~/components/ui/sidebar";
 import { Separator } from "~/components/ui/separator";
-import { AppSidebar } from "~/components/sidebar-left";
+import { BellIcon, HomeIcon, SettingsIcon } from "lucide-react";
+import { MessageCircleIcon } from "lucide-react";
+import { cn } from "~/lib/utils";
+
+const sidebarNavItems = [
+  {
+    title: "Dashboard",
+    href: "/home",
+    icon: <HomeIcon />,
+  },
+  {
+    title: "Messages",
+    href: "/messages",
+    icon: <MessageCircleIcon />,
+  },
+  {
+    title: "Notifications",
+    href: "/notifications",
+    icon: <BellIcon />,
+  },
+  {
+    title: "Settings",
+    href: "/settings",
+    icon: <SettingsIcon />,
+  },
+];
 
 export async function loader({ request }: Route.LoaderArgs) {
   return null;
@@ -18,12 +43,25 @@ export async function action({ request }: Route.ActionArgs) {
 
 export default function Page({ loaderData }: Route.ComponentProps) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
+    <div className="grid grid-cols-8 grid-flow-col mx-auto max-w-[1440px]">
+      {/* sidebar */}
+      <div className="col-span-2 bg-red-500">
+        <div className="flex flex-col gap-2 p-4">
+          {sidebarNavItems.map((e) => (
+            <NavLink
+              to={e.href}
+              className={({ isActive }) => cn(isActive && "bg-blue-500")}
+            >
+              {e.icon}
+              {e.title}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+      {/* main */}
+      <div className="col-span-6 bg-blue-500">
         <Outlet />
-      </main>
-    </SidebarProvider>
+      </div>
+    </div>
   );
 }
