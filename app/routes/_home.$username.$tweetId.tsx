868667401341db302f7bfe53b8eb996d8aa63a06
butterismac <imbxyz1@gmail.com>
2025-06-27 05:19:59 -0700

    zzz

--- app/routes/_home.$username.$tweetId.tsx
+++ app/routes/_home.$username.$tweetId.tsx
@@ -1,7 +1,79 @@
+import { Tweet } from "~/components/tweet";
 import type { Route } from "./+types/_home.$username.$tweetId";
+import { requireAuthRedirect } from "~/.server/auth";
+import { redirect } from "react-router";
+import { prisma } from "~/.server/prisma";
+import type { TweetProps } from "~/lib/types";
 
-export async function loader({ request }: Route.LoaderArgs) {
-  return null;
+export async function loader({ request, params }: Route.LoaderArgs) {
+  const auth = await requireAuthRedirect(request);
+  const { username, tweetId } = params;
+  if (!username || !tweetId) {
+    return redirect("/");
+  }
+  const tweet = await prisma.tweet.findUnique({
+    where: {
+      id: tweetId,
+    },
+    include: {
+      author: {
+        select: {
+          username: true,
+          followersCount: true,
+          followingCount: true,
+          profile: {
+            select: {
+              displayName: true,
+              avatarURL: true,
+              bio: true,
+            },
+          },
+        },
+      },
+      likes: {
+        where: {
+          tweetId: tweetId,
+          userId: auth.userId,
+        },
+      },
+      Bookmark: {
+        where: {
+          tweetId,
+          userId: auth.userId,
+        },
+      },
+      media: {
+        select: {
+          url: true,
+          type: true,
+        },
+      },
+    },
+  });
+  if (!tweet) {
+    return redirect("/");
+  }
+  let returnedTweet: TweetProps["tweet"] = {
+    id: tweet.id,
+    authorId: tweet.authorId,
+    username: tweet.author.username,
+    displayName: tweet.author.profile?.displayName ?? tweet.author.username,
+    avatarURL: tweet.author.profile?.avatarURL,
+    createdAt: tweet.createdAt,
+    content: tweet.content,
+    likeCount: tweet.likeCount,
+    hasLiked: tweet.likes.length > 0,
+    hasBookmarked: tweet.Bookmark.length > 0,
+    replyCount: tweet.replyCount,
+    quoteCount: tweet.quoteCount,
+    retweetCount: tweet.retweetCount,
+    bookmarkCount: tweet.bookmarkCount,
+    followingCount: tweet.author.followingCount,
+    followerCount: tweet.author.followersCount,
+    bio: tweet.author.profile?.bio,
+    mediaURLs: tweet.media,
+  };
+  return { returnedTweet };
 }
 
 export async function action({ request }: Route.ActionArgs) {
@@ -9,5 +81,9 @@ export async function action({ request }: Route.ActionArgs) {
 }
 
 export default function Page({ loaderData }: Route.ComponentProps) {
-  return <div>Tweet</div>;
+  return (
+    <div>
+      <Tweet tweet={loaderData.returnedTweet} />
+    </div>
+  );
 }
